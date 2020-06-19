8eacaf1e3c8acd6f27bee89327fead4be31270f3
louis <958053629@qq.com>
2020-06-19 09:38:01 +0800

    [优化代码]

--- mall-security/src/main/java/com/macro/mall/security/config/SecurityConfig.java
+++ mall-security/src/main/java/com/macro/mall/security/config/SecurityConfig.java
@@ -57,6 +57,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 // 自定义权限拦截器JWT过滤器
                 .and()
                 .addFilterBefore(jwtAuthenticationTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+
         //有动态权限配置时添加动态权限校验过滤器
         if(dynamicSecurityService!=null){
             registry.and().addFilterBefore(dynamicSecurityFilter(), FilterSecurityInterceptor.class);
